778435cb795eaed5e316dff403a518f536c099f9
seba <se.bernal@duocuc.cl>
2023-04-25 21:53:21 -0400

    ajustes metP

--- Pagina_vin/js/metpago.js
+++ Pagina_vin/js/metpago.js
@@ -1,12 +1,31 @@
+
+/* //se guarda el valor en una variable
+const nombre = document.getElementById('nomtarjnueva'); 
+
+const nombreerror = document.getElementById('nombreerror');
+
+const button = document.getElementById('button');
+
+// FUNCION FLECHA ()=>{} 
+// Esto esta pendiente de lo que pasa al realizar una accion (el click) addEventListener
+
+button.addEventListener('click', ()=>{
+    console.log('boton presionado');
+});
+
+// intento malio sal xd  */
+
 function ValidarTexto(){
     console.log("nombre de tarjeta")
-    let nom=document.getElementById("tarjnueva").value
+    let nom=document.getElementById("nomtarjnueva").value
     if (nom.length==0)
     {
-        document.getElementById("help_nom").innerHTML="Debe rellenar el campo"
+        document.getElementById("error_nom").innerHTML="Debe rellenar el campo"
         console.log("el nombre esta vacio");
     }
 }
+
+
 
 function ValidarNum_tar(){/* Incompleto */
     console.log("numero tarjeta")
